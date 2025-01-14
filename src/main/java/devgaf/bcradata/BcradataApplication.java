25f73e27899ca582ec8d13b62f2bf6c19e46dc81
Gaston Fernandez <gastonmfernandez@outlook.com>
2025-01-14 11:09:35 -0300

    Initial commit

---
+++ src/main/java/devgaf/bcradata/BcradataApplication.java
@@ -0,0 +1,13 @@
+package devgaf.bcradata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcradataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcradataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devgaf/bcradata/BcradataApplicationTests.java
@@ -0,0 +1,13 @@
+package devgaf.bcradata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcradataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
