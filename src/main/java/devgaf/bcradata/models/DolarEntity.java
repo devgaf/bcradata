66beb7fa71e098172fbd2ea2415df4f2598b529a
Gaston Fernandez <gastonmfernandez@outlook.com>
2025-01-30 00:33:22 -0300

    Creacion de entidades

--- src/main/java/devgaf/bcradata/models/DolarEntity.java
+++ src/main/java/devgaf/bcradata/models/DolarEntity.java
@@ -2,6 +2,11 @@ package devgaf.bcradata.models;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +14,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "dolar")
 public class DolarEntity {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private double purchase;
     private double sale;
--- src/main/java/devgaf/bcradata/models/IclEntity.java
+++ src/main/java/devgaf/bcradata/models/IclEntity.java
@@ -5,12 +5,21 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-
+@Entity
+@Table(name = "icl")
 public class IclEntity {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private double value;
     private LocalDate date;
 }
